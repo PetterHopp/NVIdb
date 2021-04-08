6c5e7b52b530b551ea951494fad523f83d2f6f68
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-08 08:05:38 +0200

    chore: build.R created Facilitates building and checking package

---
+++ notes/build.R
@@ -0,0 +1,63 @@
+# TEST, DOCUMENT AND BUILD NVIdb PACKAGE
+
+# Set up environment
+# rm(list = ls())    # Benyttes for å tømme R-environment ved behov
+Rlibrary <- R.home()
+
+library(devtools)
+library(roxygen2)
+library(withr)
+
+# Creates new help files
+# Should be run before git push when documentation for functions have been changed
+devtools::document()
+
+# Run tests included in ./tests. NVIdb use testthat
+devtools::test()
+
+# Build the vignette
+# devtools::build_vignettes()
+# vignetteRDS <- readRDS("./Meta/vignette.rds")
+
+# devtools::build_manual()
+
+# Build the package
+# system("R CMD build ../NVIdb")
+# devtools::build(binary = TRUE)
+devtools::build(binary = FALSE, manual = TRUE, vignettes = TRUE)
+
+version <- packageVersion("NVIdb", lib.loc = paste0(getwd(),"/.."))
+devtools::check_built(path = paste0("../NVIdb_", version, ".tar.gz"), manual = TRUE)
+
+# Extensive checking of package. Is done after build. Creates PDF-manual
+# system("R CMD check --ignore-vignettes ../NVIdb")
+# Alternative for creating the PDF-manual. The manual is not put in the correct directory
+# system(paste(shQuote(file.path(R.home("bin"), "R")),
+#              "CMD",
+#              "Rd2pdf",
+#              shQuote(paste0(Rlibrary,"/library/NVIdb"))))
+
+
+# Innstall rebuilt package
+detach("package:NVIdb", unload=TRUE)
+
+with_libpaths(paste0(Rlibrary,"/library"),
+              install(sub("notes", "", dirname(rstudioapi::getSourceEditorContext()$path)),
+                      dependencies = TRUE,
+                      upgrade=FALSE,
+                      build_vignettes = TRUE)
+)
+
+# # Install from binary file
+# remove.packages("NVIdb")
+# install.packages(pkgs = paste0(getwd(), "/..", "/NVIdb_", version, ".tar.gz"),
+#                  type = "source",
+#                  repos = NULL)
+
+# install.packages(paste0(getwd(), "/..", "/NVIdb_", version, ".zip"),
+#                  repos = NULL,
+#                  type = "binary")
+
+help(package="NVIdb")
+library(NVIdb)
+
