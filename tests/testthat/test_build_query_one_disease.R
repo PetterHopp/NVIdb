59a352e1669c7d358a6ddd619dbe91c2111233dd
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-14 08:33:46 +0200

    test: updated test_build_query_one_disease Updated correct result due to changes in sql output from build_sql_select_code and build_query_one_disease

--- tests/testthat/test_build_query_one_disease.R
+++ tests/testthat/test_build_query_one_disease.R
@@ -13,10 +13,10 @@ test_that("build query PD", {
   correct_result <-  paste("SELECT *",
                            "FROM v2_sak_m_res",
                            "WHERE aar = 2020 AND",
-                           "( hensiktkode in ('0100108018', '0100109003', '0100111003', '0800109') OR",
-                           "metodekode in ('070070', '070231', '010057', '060265') OR",
-                           "konkl_analyttkode in ('1502010235') OR konkl_analyttkode LIKE '01220104%' OR",
-                           "analyttkode_funn in ('1502010235') OR analyttkode_funn LIKE '01220104%' )")
+                           "( hensiktkode IN ('0100108018', '0100109003', '0100111003', '0800109') OR",
+                           "metodekode IN ('070070', '070231', '010057', '060265') OR",
+                           "konkl_analyttkode = '1502010235' OR konkl_analyttkode LIKE '01220104%' OR",
+                           "analyttkode_funn = '1502010235' OR analyttkode_funn LIKE '01220104%' )")
 
   expect_equivalent(query["selection_v2_sak_m_res"], correct_result)
 
@@ -27,7 +27,7 @@ test_that("build query PD", {
                            "ON (v_sakskonklusjon.aar = sak.aar AND",
                            "v_sakskonklusjon.ansvarlig_seksjon = sak.ansvarlig_seksjon AND",
                            "v_sakskonklusjon.innsendelsesnummer = sak.innsendelsesnummer)",
-                           "WHERE sak.aar = 2020 AND ( analyttkode in ('1502010235') OR analyttkode LIKE '01220104%' )")
+                           "WHERE sak.aar = 2020 AND ( analyttkode = '1502010235' OR analyttkode LIKE '01220104%' )")
 
   expect_equivalent(query["selection_sakskonklusjon"], correct_result)
 
@@ -42,9 +42,9 @@ test_that("build query CMS", {
   correct_result <-  paste("SELECT *",
                            "FROM v2_sak_m_res",
                            "WHERE aar = 2020 AND",
-                           "(  metodekode in ('070183') OR",
-                           "konkl_analyttkode in ('15020804') OR konkl_analyttkode LIKE '012601%' OR",
-                           "analyttkode_funn in ('15020804') OR analyttkode_funn LIKE '012601%' )")
+                           "( metodekode = '070183' OR",
+                           "konkl_analyttkode = '15020804' OR konkl_analyttkode LIKE '012601%' OR",
+                           "analyttkode_funn = '15020804' OR analyttkode_funn LIKE '012601%' )")
 
   expect_equivalent(query["selection_v2_sak_m_res"], correct_result)
 
@@ -55,7 +55,7 @@ test_that("build query CMS", {
                            "ON (v_sakskonklusjon.aar = sak.aar AND",
                            "v_sakskonklusjon.ansvarlig_seksjon = sak.ansvarlig_seksjon AND",
                            "v_sakskonklusjon.innsendelsesnummer = sak.innsendelsesnummer)",
-                           "WHERE sak.aar = 2020 AND ( analyttkode in ('15020804') OR analyttkode LIKE '012601%' )")
+                           "WHERE sak.aar = 2020 AND ( analyttkode = '15020804' OR analyttkode LIKE '012601%' )")
 
   expect_equivalent(query["selection_sakskonklusjon"], correct_result)
 
@@ -69,7 +69,7 @@ test_that("build query Vibrio anguillarum", {
   correct_result <-  paste("SELECT *",
                            "FROM v2_sak_m_res",
                            "WHERE aar = 2020 AND",
-                           "(    konkl_analyttkode LIKE '0406020202%' OR  analyttkode_funn LIKE '0406020202%' )")
+                           "( konkl_analyttkode LIKE '0406020202%' OR analyttkode_funn LIKE '0406020202%' )")
 
   expect_equivalent(query["selection_v2_sak_m_res"], correct_result)
 
@@ -80,7 +80,7 @@ test_that("build query Vibrio anguillarum", {
                            "ON (v_sakskonklusjon.aar = sak.aar AND",
                            "v_sakskonklusjon.ansvarlig_seksjon = sak.ansvarlig_seksjon AND",
                            "v_sakskonklusjon.innsendelsesnummer = sak.innsendelsesnummer)",
-                           "WHERE sak.aar = 2020 AND (  analyttkode LIKE '0406020202%' )")
+                           "WHERE sak.aar = 2020 AND ( analyttkode LIKE '0406020202%' )")
 
   expect_equivalent(query["selection_sakskonklusjon"], correct_result)
 
@@ -94,8 +94,8 @@ test_that("build query Gjellemykose", {
   correct_result <-  paste("SELECT *",
                            "FROM v2_sak_m_res",
                            "WHERE aar = 2020 AND",
-                           "(   konkl_analyttkode in ('0702060406') OR analyttkode_funn in ('0702060406') )")
-
+                           "( konkl_analyttkode = '0702060406' OR analyttkode_funn = '0702060406' )")
+  
   expect_equivalent(query["selection_v2_sak_m_res"], correct_result)
 
   correct_result <-  paste("SELECT v_sakskonklusjon.*, sak.mottatt_dato, sak.uttaksdato, sak.sak_avsluttet,",
@@ -105,7 +105,7 @@ test_that("build query Gjellemykose", {
                            "ON (v_sakskonklusjon.aar = sak.aar AND",
                            "v_sakskonklusjon.ansvarlig_seksjon = sak.ansvarlig_seksjon AND",
                            "v_sakskonklusjon.innsendelsesnummer = sak.innsendelsesnummer)",
-                           "WHERE sak.aar = 2020 AND ( analyttkode in ('0702060406') )")
+                           "WHERE sak.aar = 2020 AND ( analyttkode = '0702060406' )")
 
   expect_equivalent(query["selection_sakskonklusjon"], correct_result)
 
