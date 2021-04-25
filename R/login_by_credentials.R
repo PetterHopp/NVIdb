52feed449a350a6e919f395428a3038b39b69550
Petter Hopp <Petter.Hopp@vetinst.no>
2021-04-25 23:14:40 +0200

    refactor: login_by_credentials argument checking by checkmate Started rewriting argument checking using checkmate and NVIcheckmate

--- R/login_by_credentials.R
+++ R/login_by_credentials.R
@@ -8,28 +8,67 @@ login_by_credentials <- function(dbservice,
                                  dbport = NULL,
                                  dbprotocol = NULL) {
 
-  # Error handling
-  # 1. keyring package is missing
-  # Use of require is avoided as loading packages should be avoided in package functions
-  # This implies that there is no check of keyring is correctly installed
-  if (!is.element("keyring", utils::installed.packages()[, 1])) {
-    stop("Package keyring need to be installed for this function to work")
+  # ARGUMENT CHECKING ----
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  
+  # Perform checks
+  # dbservice
+  checkmate::assert_character(dbservice, min.chars = 1, len = 1, any.missing = FALSE, add = checks)
+  
+  # Identify if NVIconfig are installed and parameters for dbservice exists. 
+  # Produce extra message in case parameters are lacking
+  x_msg <- NVIcheckmate::check_package(X = "NVIconfig")
+  if (x_msg == TRUE) {
+    x_msg <- NVIcheckmate::check_choice_anycase(x = dbservice, choices = NVIconfig:::dbconnect$dbservice)
+    if (x_msg != TRUE) {
+      x_msg <- "and predifined parameters for dbservice don't exist in the installed version of NVIconfig"
+    }
   }
-
-  # 2. Credentials for dbservice are missing from the user profile
-  if (!is.element(tolower(dbservice), tolower(keyring::key_list()[, 1]))) {
-    stop(paste("Username and password for",
-               dbservice,
-               "is not available for the current user on this computer"))
+  
+  # if predefined connection parameters don't exist for dbservice
+  # TO DO: include possibility for extra message in assert_character 
+  if (x_msg != TRUE) {
+  # dbdriver
+  checkmate::assert_character(dbdriver, min.chars = 1, len = 1, any.missing = FALSE, add = checks)
+  # db
+  checkmate::assert_character(db, min.chars = 1, len = 1, any.missing = FALSE, add = checks)
+  # dbserver
+  checkmate::assert_character(dbserver, min.chars = 1, len = 1, any.missing = FALSE, add = checks)
+  # dbport
+  checkmate::assert_character(dbport, len = 1, any.missing = FALSE, add = checks)
+  # dbprotocol
+  checkmate::assert_character(dbprotocol, min.chars = 1, len = 1, any.missing = FALSE, add = checks)
   }
-
-  # 3. Parameters for db-connection is missing
-  if ((is.null(dbdriver) | is.null(db) | is.null(dbserver) | is.null(dbport) | is.null(dbprotocol)) &
-      !tolower(dbservice) %in% tolower(NVIconfig:::dbconnect$dbservice)) {
-    stop(paste("Parameters for connection to",
-               dbservice,
-               "are missing and predefined parameters are not available"))
-  }
+  
+  # credentials
+  NVIcheckmate::assert_credentials(dbservice)
+  
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+  # # Error handling
+  # # 1. keyring package is missing
+  # # Use of require is avoided as loading packages should be avoided in package functions
+  # # This implies that there is no check of keyring is correctly installed
+  # if (!is.element("keyring", utils::installed.packages()[, 1])) {
+  #   stop("Package keyring need to be installed for this function to work")
+  # }
+  # 
+  # # 2. Credentials for dbservice are missing from the user profile
+  # if (!is.element(tolower(dbservice), tolower(keyring::key_list()[, 1]))) {
+  #   stop(paste("Username and password for",
+  #              dbservice,
+  #              "is not available for the current user on this computer"))
+  # }
+  # 
+  # # 3. Parameters for db-connection is missing
+  # if ((is.null(dbdriver) | is.null(db) | is.null(dbserver) | is.null(dbport) | is.null(dbprotocol)) &
+  #     !tolower(dbservice) %in% tolower(NVIconfig:::dbconnect$dbservice)) {
+  #   stop(paste("Parameters for connection to",
+  #              dbservice,
+  #              "are missing and predefined parameters are not available"))
+  # }
 
   # Identifies connection parameters for predefined dbservices
   # Uses the predefined parameters only for parameters with NULL-value
@@ -66,6 +105,9 @@ login_by_credentials <- function(dbservice,
 
 login_by_credentials_PJS <- function() {
 
+  # ARGUMENT CHECKING 
+  # performed in login_by_credentials
+
   odbcConnection <- NVIdb::login_by_credentials(dbservice = "PJS")
 
   return(odbcConnection)
@@ -78,6 +120,9 @@ login_by_credentials_PJS <- function() {
 
 login_by_credentials_EOS <- function() {
 
+  # ARGUMENT CHECKING 
+  # performed in login_by_credentials
+  
   odbcConnection <- NVIdb::login_by_credentials(dbservice = "EOS")
 
   return(odbcConnection)
