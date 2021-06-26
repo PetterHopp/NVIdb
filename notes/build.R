f500d12791fd5b3998ac1eaf95a7a296c0901467
Petter Hopp <Petter.Hopp@vetinst.no>
2021-06-26 11:25:51 +0200

    chore: build.R and install.R standardized standardized to use pkg <- "NVIdb" all installation code is removed from build.R

--- notes/build.R
+++ notes/build.R
@@ -2,6 +2,9 @@
 
 # Set up environment
 # rm(list = ls())    # Benyttes for å tømme R-environment ved behov
+
+pkg <- "NVIdb"
+
 Rlibrary <- R.home()
 
 library(devtools)
@@ -12,53 +15,31 @@ library(withr)
 # Should be run before git push when documentation for functions have been changed
 devtools::document()
 
-# Run tests included in ./tests. NVIdb use testthat
+# Alternative for creating the PDF-manual. The manual is not put in the correct directory
+# system(paste(shQuote(file.path(R.home("bin"), "R")),
+#              "CMD",
+#              "Rd2pdf",
+#              paste0("../", pkg)))
+# file.copy(from = "NVIdb.pdf", to = "./vignettes", overwrite = TRUE)
+# file.remove(".Rd2pdf16372")
+# file.remove("NVIdb.pdf")
+
+# Run tests included in ./tests.
 devtools::test()
 
 # Build the vignette
 # devtools::build_vignettes()
 # vignetteRDS <- readRDS("./Meta/vignette.rds")
 
-# devtools::build_manual()
+# devtools::build_manual(pkg = "../NVIdb", path = "./vignettes")
 
 # Build the package
 # system("R CMD build ../NVIdb")
 # devtools::build(binary = TRUE)
 devtools::build(binary = FALSE, manual = TRUE, vignettes = TRUE)
 
-version <- packageVersion("NVIdb", lib.loc = paste0(getwd(),"/.."))
-# devtools::check_built(path = paste0("../NVIdb_", version, ".tar.gz"), manual = TRUE)
-devtools::check_built(path = paste0("../NVIdb_", version, ".tar.gz"), args = c("--no-tests"), manual = TRUE)
+version <- packageVersion(pkg, lib.loc = paste0(getwd(),"/.."))
+devtools::check_built(path = paste0("../", pkg, "_", version, ".tar.gz"), args = c("--no-tests"), manual = TRUE)
 
 # Extensive checking of package. Is done after build. Creates PDF-manual
 # system("R CMD check --ignore-vignettes ../NVIdb")
-# Alternative for creating the PDF-manual. The manual is not put in the correct directory
-# system(paste(shQuote(file.path(R.home("bin"), "R")),
-#              "CMD",
-#              "Rd2pdf",
-#              shQuote(paste0(Rlibrary,"/library/NVIdb"))))
-
-
-# Innstall rebuilt package
-detach("package:NVIdb", unload=TRUE)
-
-with_libpaths(paste0(Rlibrary,"/library"),
-              install(sub("notes", "", dirname(rstudioapi::getSourceEditorContext()$path)),
-                      dependencies = TRUE,
-                      upgrade=FALSE,
-                      build_vignettes = TRUE)
-)
-
-# # Install from binary file
-# remove.packages("NVIdb")
-# install.packages(pkgs = paste0(getwd(), "/..", "/NVIdb_", version, ".tar.gz"),
-#                  type = "source",
-#                  repos = NULL)
-
-# install.packages(paste0(getwd(), "/..", "/NVIdb_", version, ".zip"),
-#                  repos = NULL,
-#                  type = "binary")
-
-help(package="NVIdb")
-library(NVIdb)
-
