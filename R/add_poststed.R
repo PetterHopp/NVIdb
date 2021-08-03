7e9d584e9d298292eb4c0ce2d06e42b582a5cbe9
Petter Hopp <Petter.Hopp@vetinst.no>
2021-08-03 14:54:28 +0200

    refactor: add_poststed improved argument checking Use assert_add_function for agument checking

--- R/add_poststed.R
+++ R/add_poststed.R
@@ -82,91 +82,14 @@ add_poststed <- function(data,
   new_column <- set_name_vector(new_column)
   
   # ARGUMENT CHECKING ----
-  # Object to store check-results
-  checks <- checkmate::makeAssertCollection()
+  assert_add_function(data = data,
+                      translation_table = translation_table,
+                      code_column = code_column,
+                      new_column = new_column,
+                      position = position,
+                      overwrite = overwrite)
   
-  # Perform checks
-  # data
-  checkmate::assert_data_frame(data, add = checks)
-  # translation_table
-  checkmate::assert_data_frame(translation_table, add = checks)
-  # code_column
-  checkmate::assert_vector(code_column, any.missing = FALSE, len = 1, add = checks)
-  NVIcheckmate::assert_names(unname(code_column), 
-                             type = "named", 
-                             subset.of = colnames(translation_table), 
-                             comment = paste0("The value for code_column must be a column in the translation table '",
-                                              deparse(substitute(translation_table)),
-                                              "' but '",
-                                              unname(code_column), 
-                                              "' is not a column name in the translation table"),
-                             add = checks)
-  NVIcheckmate::assert_names(names(code_column), 
-                             type = "named", 
-                             subset.of = colnames(data), 
-                             comment = paste0("The name of the code_column must be a column in the data '",
-                                              deparse(substitute(data)),
-                                              "' but '",
-                                              base::setdiff(names(code_column), colnames(data)), 
-                                              "' is not a column in the data. You must name the code_column ",
-                                              "if the code_column in data is different from in the translation table"),
-                             add = checks)
-  # new_column
-  checkmate::assert_vector(new_column, any.missing = FALSE, min.len = 1, add = checks)
-  NVIcheckmate::assert_names(unname(new_column), 
-                             type = "named", 
-                             subset.of = colnames(translation_table), 
-                             comment = paste0("The values for new_column must be column names in the translation table '",
-                                              deparse(substitute(translation_table)),
-                                              "' but '",
-                                              unname(new_column), 
-                                              "' are not column names in the translation table"),
-                             add = checks)
-  if (isFALSE(overwrite)) {
-    NVIcheckmate::assert_names(names(new_column), 
-                               type = "named", 
-                               disjunct.from = setdiff(colnames(data), code_column),
-                               comment = paste0("The column name(s): '",
-                                                intersect(colnames(data), names(new_column)),
-                                                "' already exist in '",
-                                                deparse(substitute(data)),
-                                                "`. Either give new column name(s) for the column(s) called '", 
-                                                intersect(colnames(data), names(new_column)),
-                                                "' or specify overwrite = TRUE to replace values in the existing column(s) with new content"),
-    add = checks)
-  }
-  NVIcheckmate::assert_names(names(new_column), 
-                             type = "named", 
-                             disjunct.from = names(code_column), 
-                             comment = paste0("You cannot give the new column the same name as the code_column '",
-                                              names(code_column),
-                                              "' in the data '",
-                                              deparse(substitute(data)), "`"),
-                             add = checks)
-  # position
-  checkmate::assert_choice(position, choices = c("first", "left", "right", "last", "keep"), add = checks)
-  # overwrite
-  checkmate::assert_logical(overwrite, any.missing = FALSE, len = 1, add = checks)
-  
-  
-  # Report check-results
-  checkmate::reportAssertions(checks)
-  
-  # # ERROR check
-  # # error:
-  # if (names(code_column) %in% names(new_column)) {
-  #   # issue error if names already exists
-  #   stop(paste0("You cannot give the new column the same name as the code_column '", names(code_column), "' in the data frame '", deparse(substitute(data)), "`."))
-  # }
-  # 
-  # # check_exist_colname(df_name = deparse(substitute(data)), df_columns = colnames(data), new_column = new_column, overwrite = overwrite)
-  # if (length(intersect(colnames(data), names(new_column))) > 0 & overwrite == FALSE) {
-  #   # issue error if names already exists
-  #   stop(paste(paste0("The column name(s): '", intersect(colnames(data), names(new_column)), "' already exist in '", deparse(substitute(data)), "`."),
-  #              paste0("Either give new column name(s) for the column(s) called '", intersect(colnames(data), names(new_column)), "' or"),
-  #              "Specify overwrite = TRUE to replace values in the existing column(s) with new content.", sep = "\n"))
-  # }
-  
+
   # Makes the translation table with code_column and new_column. unique() is necessary to avoid duplicate
   # rows when code_column is not "komnr"
   code_2_new <- unique(translation_table[, c(unname(code_column), unname(new_column))])
