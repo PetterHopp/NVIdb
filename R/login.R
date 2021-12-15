72d6c00665a4d63c2822575ae99099c8c154b91a
Petter Hopp <Petter.Hopp@vetinst.no>
2021-12-15 08:39:52 +0100

    fix: Updated login, login_by_input and login_by_credentials to NVIconfig::dbconnect

--- R/login.R
+++ R/login.R
@@ -82,7 +82,7 @@ login <- function(dbservice,
 
   # 3. Parameters for db-connection is missing
   if ((is.null(dbdriver) | is.null(db) | is.null(dbserver) | is.null(dbport) | is.null(dbprotocol)) &
-      !tolower(dbservice) %in% tolower(NVIconfig:::dbconnect$dbservice)) {
+      !tolower(dbservice) %in% tolower(NVIconfig::dbconnect$dbservice)) {
     stop(paste("Parameters for connection to",
                dbservice,
                "are missing and predefined parameters are not available"))
@@ -91,7 +91,7 @@ login <- function(dbservice,
   # Identifies connection parameters for predefined dbservices
   # Uses the predefined parameters only for parameters with NULL-value
   if (is.null(dbdriver) | is.null(db) | is.null(dbserver) | is.null(dbport) | is.null(dbprotocol)) {
-    connect <- NVIconfig:::dbconnect[tolower(dbservice), ]
+    connect <- NVIconfig::dbconnect[tolower(dbservice), ]
     if (is.null(dbdriver))   {dbdriver   <- connect[, "dbdriver"]}
     if (is.null(db))         {db         <- connect[, "db"]}
     if (is.null(dbserver))   {dbserver   <- connect[, "dbserver"]}
--- R/login_by_input.R
+++ R/login_by_input.R
@@ -12,7 +12,7 @@ login_by_input <- function(dbservice,
   # Error handling
   # 1. Parameters for db-connection is missing
   if ((is.null(dbdriver) | is.null(db) | is.null(dbserver) | is.null(dbport) | is.null(dbprotocol)) &
-      !tolower(dbservice) %in% tolower(NVIconfig:::dbconnect$dbservice)) {
+      !tolower(dbservice) %in% tolower(NVIconfig::dbconnect$dbservice)) {
     stop(paste("Parameters for connection to",
                dbservice,
                "are missing and predefined parameters are not available"))
@@ -21,7 +21,7 @@ login_by_input <- function(dbservice,
   # Identifies connection parameters for predefined dbservices
   # Uses the predefined parameters only for parameters with NULL-value
   if (is.null(dbdriver) | is.null(db) | is.null(dbserver) | is.null(dbport) | is.null(dbprotocol)) {
-    connect <- NVIconfig:::dbconnect[tolower(dbservice), ]
+    connect <- NVIconfig::dbconnect[tolower(dbservice), ]
     if (is.null(dbdriver))   {dbdriver   <- connect[, "dbdriver"]}
     if (is.null(db))         {db         <- connect[, "db"]}
     if (is.null(dbserver))   {dbserver   <- connect[, "dbserver"]}
