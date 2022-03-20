d8d3051ad4c8c422f30a469d1c09e722c26ed25b
Petter Hopp <petter.hopp@vetinst.no>
2022-03-20 13:37:26 +0100

    refactor: copy_poststed improved handling of  ending "/" in path

--- R/copy_poststed.R
+++ R/copy_poststed.R
@@ -4,25 +4,23 @@
 copy_poststed <- function(filename = "Poststed_UTF8.csv",
                           from_path = paste0(set_dir_NVI("GrunndataLand"), "FormaterteData/"),
                           to_path = NULL) {
-
+  
   # ARGUMENT CHECKING ----
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
+  
   # Perform checks
   # filename
   checkmate::assert_character(filename, len = 1, min.chars = 1, add = checks)
   # from_path / filename
-  checkmate::assert_file_exists(paste0(from_path, filename), access = "r", add = checks)
+  checkmate::assert_file_exists(file.path(sub("/$", "", from_path), filename), access = "r", add = checks)
   # to_path
-  if (endsWith(to_path, "/")) {
-    checkmate::assert_directory_exists(substr(to_path, 1, nchar(to_path) - 1), access = "r", add = checks)
-  } else {
-    checkmate::assert_directory_exists(to_path, access = "r", add = checks)
-  }
+  checkmate::assert_directory_exists(sub("/$", "", to_path), access = "r", add = checks)
+  
   # Report check-results
   checkmate::reportAssertions(checks)
-
+  
   # COPY FILE ----
   copy_file_if_updated(filename = filename, from_path = from_path, to_path = to_path)
-
+  
 }
