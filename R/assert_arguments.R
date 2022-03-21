83e87f941b4a09044a2d4f14c58b0fba804bf7b0
Petter Hopp <PetterHopp@users.noreply.github.com>
2022-03-21 19:33:35 +0100

    Update assert_arguments.R
    Create assert_copy_function

--- R/assert_arguments.R
+++ R/assert_arguments.R
@@ -103,3 +103,54 @@
   checkmate::reportAssertions(checks)
   
 }
+
+#' @title Collection of assertions for copy_functions 
+#' @description Collection of assertions used in standard copy_functions. 
+#'
+#' @details All copy functions have the same arguments and the 
+#'     assertion can be standardized. 
+#'
+#' @param filename Argument to the add-function to be asserted. 
+#' @param from_path Argument to the add-function to be asserted. 
+#' @param to_path Argument to the add-function to be asserted. 
+#'
+#' @return \code{TRUE } if none of the assertions failed. If any of the assertions 
+#'     failed, one or more error messages are returned. 
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @keywords internal
+#'
+  assert_copy_function <- function(filename ,
+                                  from_path,
+                                  to_path) {
+    
+  # ARGUMENT CHECKING ----
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  
+  # Perform checks
+  # filename
+  checkmate::assert_data_frame(data, add = checks)
+    # filename
+  checkmate::assert_list(filename, len = 2, add = checks)
+  # # from_path
+  # checkmate::assert_character(from_path, len = 1, min.chars = 1, add = checks)
+  # if (endsWith(from_path, "/")) {
+  #   checkmate::assert_directory_exists(substr(from_path, 1, nchar(from_path) - 1), access = "r", add = checks)
+  # } else {
+  #   checkmate::assert_directory_exists(from_path, access = "r", add = checks)
+  # }
+  # from_path / filename
+  for (i in c(1:length(filename))) {
+    checkmate::assert_file_exists(paste0(from_path, filename[[i]]), access = "r", add = checks)
+  }
+  # to_path
+  if (endsWith(to_path, "/")) {
+    checkmate::assert_directory_exists(substr(to_path, 1, nchar(to_path) - 1), access = "r", add = checks)
+  } else {
+    checkmate::assert_directory_exists(to_path, access = "r", add = checks)
+  }  
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+}
