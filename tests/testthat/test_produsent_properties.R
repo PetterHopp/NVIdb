4913b113e7797eb7a4de15b338af80b4d19af650
Petter Hopp <Petter.Hopp@vetinst.no>
2022-03-24 07:25:26 +0100

    test: edited test_produsent_properties.R

--- tests/testthat/test_produsent_properties.R
+++ tests/testthat/test_produsent_properties.R
@@ -7,25 +7,30 @@ td <- tempdir()
 if (!dir.exists(file.path (td, "test"))) {
   dir.create(file.path (td, "test")) 
 } 
-filenames <- "Poststed_UTF8.csv"
+filenames <- "produsent_UTF8.csv"
 if (file.exists(file.path (td, "test", filenames[1]))) {
   file.remove(file.path (td, "test", filenames[1])) 
 } 
 
-test_that("Copy poststed", {
+test_that("Copy produsent", {
   # skip if no connection to 'FAG' have been established
   skip_if_not(dir.exists(set_dir_NVI("FAG")))
   
   # copy file
-  copy_poststed(to_path = td)
+  copy_produsent(to_path = td)
   expect_true(file.exists(file.path(td, filenames[1])))
   
-  copy_poststed(from_path = td, to_path =  file.path (td, "test"))
+  copy_produsent(from_path = td, to_path =  file.path (td, "test"))
   expect_true(file.exists(file.path(td, "test", filenames[1])))
 
 })
 
-# From add_produsent_help
+
+test_that("Correct merging of produsent og komnr", {
+  # skip if no connection to 'FAG' have been established
+  skip_if_not(dir.exists(set_dir_NVI("FAG")))
+  
+  # From add_produsent_help
 #CURRENT PRODNR8
 # Reading from standard directory at NVI's network
 prodnr_2_gjeldende_prodnr <- read_prodnr_2_current_prodnr()
@@ -55,31 +60,31 @@ newdata <- add_produsent(newdata,
                          code_column = "prodnr8",
                          new_column = c("longitude" = "geo_eu89_o", "latutude" = "geo_eu89_n"))
 # End of From add_produsent_help
-                         
+})
 
 
-test_that("Correct merging of poststed og komnr", {
+test_that("Correct merging of produsent og komnr", {
   # skip if no connection to 'FAG' have been established
   skip_if_not(dir.exists(set_dir_NVI("FAG")))
 
-  # Load translation table for poststed
-  poststed <- read_poststed()
+  # Load translation table for produsent
+  produsent <- read_produsent()
 
   # Make a dataframe with postnr that should be translated
-  poststeder <- as.data.frame(c("0468", "1343", "4550", "7800"))
-  colnames(poststeder) <- "postnr"
+  produsenter <- as.data.frame(c("0468", "1343", "4550", "7800"))
+  colnames(produsenter) <- "postnr"
 
   # Make a dataframe with the correct result
-  correct_result <- cbind(poststeder,
+  correct_result <- cbind(produsenter,
                           as.data.frame(c("OSLO", "EIKSMARKA", "FARSUND", "NAMSOS"), stringsAsFactors = FALSE),
                           as.data.frame(c("0301", "0219", "4206", "5007"), stringsAsFactors = FALSE))
-  colnames(correct_result) <- c("postnr", "poststed", "komnr")
+  colnames(correct_result) <- c("postnr", "produsent", "komnr")
 
   # Compare Add fylke, current fylkenr and current fylke with correct result
-  expect_identical(add_poststed(poststeder,
-                                translation_table = poststed,
+  expect_identical(add_produsent(produsenter,
+                                translation_table = produsent,
                                 code_column = "postnr",
-                                new_column = c("poststed", "komnr"),
+                                new_column = c("produsent", "komnr"),
                                 position = "last"),
                    correct_result)
 
@@ -90,56 +95,56 @@ test_that("Correct result when using overwrite and keep", {
   # skip if no connection to 'FAG' have been established
   skip_if_not(dir.exists(set_dir_NVI("FAG")))
 
-  # Load translation table for poststed
-  poststed <- read_poststed()
+  # Load translation table for produsent
+  produsent <- read_produsent()
 
   # Make a dataframe with postnr that should be translated
-  poststeder <- as.data.frame(c("0468", "1343", "4550", "7800"))
-  colnames(poststeder) <- "postnr"
+  produsenter <- as.data.frame(c("0468", "1343", "4550", "7800"))
+  colnames(produsenter) <- "postnr"
 
   # Make a dataframe with the correct result
-  correct_result <- cbind(poststeder,
+  correct_result <- cbind(produsenter,
                           as.data.frame(c("OSLO", "EIKSMARKA", "FARSUND", "NAMSOS"), stringsAsFactors = FALSE),
                           as.data.frame(c("0301", "0219", "4206", "5007"), stringsAsFactors = FALSE))
-  colnames(correct_result) <- c("postnr", "poststed", "komnr")
+  colnames(correct_result) <- c("postnr", "produsent", "komnr")
 
-  poststeder <- add_poststed(poststeder,
-                             translation_table = poststed,
+  produsenter <- add_produsent(produsenter,
+                             translation_table = produsent,
                              code_column = "postnr",
-                             new_column = c("poststed", "komnr"))
+                             new_column = c("produsent", "komnr"))
 
   # Compare Add fylke, current fylkenr and current fylke with correct result
-  expect_identical(poststeder,
+  expect_identical(produsenter,
                    correct_result)
 
-  poststeder <- add_poststed(poststeder,
-                             translation_table = poststed,
+  produsenter <- add_produsent(produsenter,
+                             translation_table = produsent,
                              code_column = "postnr",
-                             new_column = c("poststed", "komnr"),
+                             new_column = c("produsent", "komnr"),
                              position = "first",
                              overwrite = TRUE)
 
 
-  expect_identical(poststeder,
-                   correct_result[, c("poststed", "komnr", "postnr")])
+  expect_identical(produsenter,
+                   correct_result[, c("produsent", "komnr", "postnr")])
 
   # Compare Add kommune, with overwrite = TRUE, keep position
-  poststeder <- add_poststed(poststeder,
-                             translation_table = poststed,
+  produsenter <- add_produsent(produsenter,
+                             translation_table = produsent,
                              code_column = "postnr",
-                             new_column = c("poststed", "komnr"),
+                             new_column = c("produsent", "komnr"),
                              position = "keep",
                              overwrite = TRUE)
 
 
-  expect_identical(poststeder,
-                   correct_result[, c("poststed", "komnr", "postnr")])
+  expect_identical(produsenter,
+                   correct_result[, c("produsent", "komnr", "postnr")])
 
 
 
 })
 
-test_that("errors for add_poststed", {
+test_that("errors for add_produsent", {
   
   # skip if no connection to 'FAG' have been established
   skip_if_not(dir.exists(set_dir_NVI("FAG")))
@@ -147,23 +152,23 @@ test_that("errors for add_poststed", {
   linewidth <- options("width")
   options(width = 80)
   
-  # Load translation table for poststed
-  poststed <- read_poststed()
+  # Load translation table for produsent
+  produsent <- read_produsent()
   
-  expect_error(add_poststed(data = "no_data", translation_table = "poststed") ,
+  expect_error(add_produsent(data = "no_data", translation_table = "produsent") ,
                regexp = "Variable 'data': Must be of type 'data.frame', not 'character'.")
   
   
-  expect_error(add_poststed(data = "no_data", translation_table = "poststed", position = "before") ,
+  expect_error(add_produsent(data = "no_data", translation_table = "produsent", position = "before") ,
                regexp = "Variable 'position': Must be element")
   
-  expect_error(add_poststed(data = "no_data", translation_table = "poststed", overwrite = 1) ,
+  expect_error(add_produsent(data = "no_data", translation_table = "produsent", overwrite = 1) ,
                regexp = "Variable 'overwrite': Must be of type 'logical', not 'double'.")
   
   options(width = unlist(linewidth))
 })
 
-test_that("errors for copy_poststed", {
+test_that("errors for copy_produsent", {
   
   linewidth <- options("width")
   options(width = 80)
@@ -171,33 +176,33 @@ test_that("errors for copy_poststed", {
   # # skip if no connection to 'FAG' have been established
   # skip_if_not(dir.exists(set_dir_NVI("FAG")))
   # 
-  # # Load translation table for poststed
-  # poststed <- read_poststed()
+  # # Load translation table for produsent
+  # produsent <- read_produsent()
   # 
-  expect_error(copy_poststed(filename = NULL, from_path = tempdir(), to_path = "./") ,
+  expect_error(copy_produsent(filename = NULL, from_path = tempdir(), to_path = "./") ,
                regexp = "(filename): Must be of type 'character', *\n * not 'NULL'",
                fixed = TRUE)
   
-  expect_error(copy_poststed(filename = "filename.csv", from_path = tempdir(), to_path = "./") ,
+  expect_error(copy_produsent(filename = "filename.csv", from_path = tempdir(), to_path = "./") ,
                regexp = "File does not exist:")
   
-  expect_error(copy_poststed(filename = "filename.csv", from_path = tempdir(), to_path = "filepath_does_not_exist") ,
+  expect_error(copy_produsent(filename = "filename.csv", from_path = tempdir(), to_path = "filepath_does_not_exist") ,
                regexp = "Directory 'filepath_does_not_exist' does not\n * exists.",
                fixed = TRUE)
   
   options(width = unlist(linewidth))
 })
 
-test_that("errors for read_poststed", {
+test_that("errors for read_produsent", {
   
   linewidth <- options("width")
   options(width = 80)
   
-  expect_error(read_poststed(filename = NULL, from_path = tempdir()) ,
+  expect_error(read_produsent(filename = NULL, from_path = tempdir()) ,
                regexp = "(filename): Must be of type 'character', *\n * not 'NULL'",
                fixed = TRUE)
   
-  expect_error(read_poststed(filename = "filename.csv", from_path = tempdir()) ,
+  expect_error(read_produsent(filename = "filename.csv", from_path = tempdir()) ,
                regexp = "File does not exist:")
   
   options(width = unlist(linewidth))
