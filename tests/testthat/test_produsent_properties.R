e8f4ba290d36ca271b155fd2473158ef801fd937
Petter Hopp <Petter.Hopp@vetinst.no>
2022-04-21 08:36:26 +0200

    style: test_produsent_properties styled with Ctrl+I

--- tests/testthat/test_produsent_properties.R
+++ tests/testthat/test_produsent_properties.R
@@ -9,9 +9,9 @@ td <- tempdir()
 # } 
 filenames <- c("Prodnr2GjeldendeProdnr.csv", "Prodnr2Koordinater.csv")
 for (i in 1:length(filenames)) {
-if (file.exists(file.path (td, filenames[i]))) {
-  file.remove(file.path (td, filenames[i])) 
-} 
+  if (file.exists(file.path (td, filenames[i]))) {
+    file.remove(file.path (td, filenames[i])) 
+  } 
 }
 
 test_that("Copy produsent properties registers", {
@@ -24,7 +24,7 @@ test_that("Copy produsent properties registers", {
   
   # copy_produsent(from_path = td, to_path =  file.path (td, "data"))
   # expect_true(file.exists(file.path(td, "data", filenames[1])))
-
+  
 })
 
 
@@ -33,45 +33,45 @@ test_that("Correct merging of produsent og komnr", {
   skip_if_not(dir.exists(set_dir_NVI("FAG")))
   
   # From add_produsent_help
-#CURRENT PRODNR8
-# Reading from the temporary directory
-prodnr_2_gjeldende_prodnr <- read_prodnr_2_current_prodnr(from_path = td)
-
-prodnr8 <- c("09140087", "14260856", "17020818", "50060129")
-produsenter <- as.data.frame(prodnr8)
-
-# Make a dataframe with the correct result
-correct_result <- cbind(as.data.frame(c("42130063", "46440597", "50060129", "50060129"), stringsAsFactors = FALSE),
-                        produsenter)
-colnames(correct_result) <- c("gjeldende_prodnr8", "prodnr8")
-
-
-# Compare Add fylke, current fylkenr and current fylke with correct result
-produsenter <- add_produsent(produsenter,
+  #CURRENT PRODNR8
+  # Reading from the temporary directory
+  prodnr_2_gjeldende_prodnr <- read_prodnr_2_current_prodnr(from_path = td)
+  
+  prodnr8 <- c("09140087", "14260856", "17020818", "50060129")
+  produsenter <- as.data.frame(prodnr8)
+  
+  # Make a dataframe with the correct result
+  correct_result <- cbind(as.data.frame(c("42130063", "46440597", "50060129", "50060129"), stringsAsFactors = FALSE),
+                          produsenter)
+  colnames(correct_result) <- c("gjeldende_prodnr8", "prodnr8")
+  
+  
+  # Compare Add fylke, current fylkenr and current fylke with correct result
+  produsenter <- add_produsent(produsenter,
                                translation_table = prodnr_2_gjeldende_prodnr,
                                code_column = "prodnr8",
                                new_column = "gjeldende_prodnr8",
                                position = "left")
-expect_identical(produsenter, correct_result)
-
-
-# COORDINATES
-# Reading from standard directory at NVI's network
-prodnr_2_koordinater <- read_prodnr_2_coordinates()
-
-# Make a dataframe with the correct result
-correct_result <- cbind(produsenter,
-                        as.data.frame(c("8.78182485813634", "7.04026092117579", "11.64981213930787", "11.64981213930787"), stringsAsFactors = FALSE),
-                        as.data.frame(c("58.63237920843709", "61.47787583826138", "64.13066637524832", "64.13066637524832"), stringsAsFactors = FALSE)
-                        )
-colnames(correct_result) <- c("gjeldende_prodnr8", "prodnr8", "longitude", "latitude")
-
-produsenter <- add_produsent(produsenter,
-                         translation_table = prodnr_2_koordinater,
-                         code_column = c("gjeldende_prodnr8" = "prodnr8"),
-                         new_column = c("longitude" = "geo_eu89_o", "latitude" = "geo_eu89_n"),
+  expect_identical(produsenter, correct_result)
+  
+  
+  # COORDINATES
+  # Reading from standard directory at NVI's network
+  prodnr_2_koordinater <- read_prodnr_2_coordinates()
+  
+  # Make a dataframe with the correct result
+  correct_result <- cbind(produsenter,
+                          as.data.frame(c("8.78182485813634", "7.04026092117579", "11.64981213930787", "11.64981213930787"), stringsAsFactors = FALSE),
+                          as.data.frame(c("58.63237920843709", "61.47787583826138", "64.13066637524832", "64.13066637524832"), stringsAsFactors = FALSE)
+  )
+  colnames(correct_result) <- c("gjeldende_prodnr8", "prodnr8", "longitude", "latitude")
+  
+  produsenter <- add_produsent(produsenter,
+                               translation_table = prodnr_2_koordinater,
+                               code_column = c("gjeldende_prodnr8" = "prodnr8"),
+                               new_column = c("longitude" = "geo_eu89_o", "latitude" = "geo_eu89_n"),
                                position = "last")
-expect_identical(produsenter, correct_result)
+  expect_identical(produsenter, correct_result)
 })
 
 
