7ce36b49d4ba9cbf931615b455cce617764f96e1
Petter Hopp <Petter.Hopp@vetinst.no>
2022-04-21 07:53:36 +0200

    test: test_produsent_properties includes test for add_produsent

--- tests/testthat/test_produsent_properties.R
+++ tests/testthat/test_produsent_properties.R
@@ -34,31 +34,44 @@ test_that("Correct merging of produsent og komnr", {
   
   # From add_produsent_help
 #CURRENT PRODNR8
-# Reading from standard directory at NVI's network
-prodnr_2_gjeldende_prodnr <- read_prodnr_2_current_prodnr()
-
-# Reading from the subdirectory Data below the working directory
+# Reading from the temporary directory
 prodnr_2_gjeldende_prodnr <- read_prodnr_2_current_prodnr(from_path = td)
 
 prodnr8 <- c("09140087", "14260856", "17020818", "50060129")
-olddata <- as.data.frame(prodnr8)
+produsenter <- as.data.frame(prodnr8)
 
-# Add new column with current prodnr8
-newdata <- add_produsent(olddata,
-                         translation_table = prodnr_2_gjeldende_prodnr,
-                         code_column = "prodnr8",
-                         new_column = "gjeldende_prodnr8",
-                         position = "left")
+# Make a dataframe with the correct result
+correct_result <- cbind(as.data.frame(c("42130063", "46440597", "50060129", "50060129"), stringsAsFactors = FALSE),
+                        produsenter)
+colnames(correct_result) <- c("gjeldende_prodnr8", "prodnr8")
+
+
+# Compare Add fylke, current fylkenr and current fylke with correct result
+produsenter <- add_produsent(produsenter,
+                               translation_table = prodnr_2_gjeldende_prodnr,
+                               code_column = "prodnr8",
+                               new_column = "gjeldende_prodnr8",
+                               position = "left")
+expect_identical(produsenter, correct_result)
+
 
 # COORDINATES
 # Reading from standard directory at NVI's network
 prodnr_2_koordinater <- read_prodnr_2_coordinates()
 
-newdata <- add_produsent(newdata,
+# Make a dataframe with the correct result
+correct_result <- cbind(produsenter,
+                        as.data.frame(c("8.78182485813634", "7.04026092117579", "11.64981213930787", "11.64981213930787"), stringsAsFactors = FALSE),
+                        as.data.frame(c("58.63237920843709", "61.47787583826138", "64.13066637524832", "64.13066637524832"), stringsAsFactors = FALSE)
+                        )
+colnames(correct_result) <- c("gjeldende_prodnr8", "prodnr8", "longitude", "latitude")
+
+produsenter <- add_produsent(produsenter,
                          translation_table = prodnr_2_koordinater,
-                         code_column = "prodnr8",
-                         new_column = c("longitude" = "geo_eu89_o", "latutude" = "geo_eu89_n"))
-# End of From add_produsent_help
+                         code_column = c("gjeldende_prodnr8" = "prodnr8"),
+                         new_column = c("longitude" = "geo_eu89_o", "latitude" = "geo_eu89_n"),
+                               position = "last")
+expect_identical(produsenter, correct_result)
 })
 
 
