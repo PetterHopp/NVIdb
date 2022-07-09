39172c90fef44d6a9e329cbc041c47e4a14944b5
Petter Hopp <petter.hopp@vetinst.no>
2022-07-09 09:04:25 +0200

    performance: removed argument year from set_disease_parameters

--- R/set_disease_parameters.R
+++ R/set_disease_parameters.R
@@ -16,16 +16,13 @@
 #'     of the input analytter as a result. If sub-codes of analytt or hensikt
 #'     should be included, end the code with \%.
 #'
-#'     The selection parameters can be input values for dedicated arguments. The
-#'     argument year must always be input that way. For input parameters
+#'     The selection parameters can be input values for dedicated arguments. For input parameters
 #'     \code{hensikt2select}, \code{utbrudd2select}, \code{metode2select}, and
 #'     \code{analytt2select}, the input may be given in a source file. This may be handy if the
 #'     selection will be performed many times. It also gives the possibility of
 #'     using a for loop that selects PJS-data and performs similar analyses at one
 #'     disease at a time.
 #'
-#' @param year One year or a vector with years giving the first and last years
-#'     that should be selected as integer.
 #' @param hensikt2select Vector with specific hensikter. If sub-hensikter should
 #'     be included, end the code with \%. Can be \code{NULL}.
 #' @param utbrudd2select String with an utbrudd ID. Can be \code{NULL}.
@@ -44,13 +41,11 @@
 #' @examples
 #' # Selection parameters for Pancreatic disease (PD)
 #' selection_parameters <- set_disease_selection_parameters(
-#'   year = 2020,
 #'   analytt2select = c("01220104%", "1502010235"),
 #'   hensikt2select = c("0100108018", "0100109003", "0100111003", "0800109"),
 #'   metode2select = c("070070", "070231", "010057", "060265")
 #'   )
-set_disease_selection_parameters <- function(year = NULL,
-                                             hensikt2select = NULL,
+set_disease_parameters <- function(hensikt2select = NULL,
                                              utbrudd2select = NULL,
                                              metode2select = NULL,
                                              analytt2select = NULL,
@@ -61,7 +56,6 @@ set_disease_selection_parameters <- function(year = NULL,
   checks <- checkmate::makeAssertCollection()
 
   # Perform checks
-  checkmate::assert_integerish(year, lower = 1990, upper = as.numeric(format(Sys.Date(), "%Y")), min.len = 1, add = checks)
   checkmate::assert_character(hensikt2select, min.chars = 2, null.ok = TRUE, add = checks)
   checkmate::assert_character(utbrudd2select, null.ok = TRUE, add = checks)
   checkmate::assert_character(metode2select, min.chars = 6, null.ok = TRUE, add = checks)
@@ -78,20 +72,9 @@ set_disease_selection_parameters <- function(year = NULL,
   # Import values from parameter file
 
 
-  # These will overwrite any values given separately except year
-  if (!is.null(file)) {
-    # Avoid overwriting of input year
-    yr <- year
-    # read source file
-    source(file = file)
-    # Ensures that any year in source file is overwritten.
-    year <- yr
-  }
-
-
   # Create list object with parameter values
   return(list("year" = year,
-              "hensikt2select" = hensikt2select,
+              "hensikt2select" =  hensikt2select,
               "utbrudd2select" = utbrudd2select,
               "metode2select" = metode2select,
               "analytt2select" = analytt2select))
