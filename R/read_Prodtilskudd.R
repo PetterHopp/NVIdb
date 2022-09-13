a5ba2c4eb29ea1a0ddeb7100d39660352c830954
Petter Hopp <Petter.Hopp@vetinst.no>
2022-09-13 13:59:18 +0200

    refactor: improved argument checking in read_Prodtilskudd

--- R/read_Prodtilskudd.R
+++ R/read_Prodtilskudd.R
@@ -46,23 +46,23 @@ read_Prodtilskudd <- function(from_path = paste0(set_dir_NVI("Prodtilskudd"), "F
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
   # Perform checks
+  # from_path
   checkmate::assert_character(from_path, len = 1, min.chars = 1, add = checks)
-  # if (endsWith(from_path, "/")) {
-  #   checkmate::assert_directory_exists(substr(from_path, 1, nchar(from_path) - 1), access = "r", add = checks)
-  # } else {
   checkmate::assert_directory_exists(from_path, access = "r", add = checks)
-  # }
+  # Pkode_month
   checkmate::assert_subset(Pkode_month, choices = c("both", "last", "01", "03", "05", "07", "10", "12"), add = checks)
-  # Report check-results
-  checkmate::reportAssertions(checks)
-
+  # Pkode_year
   checkmate::assert(checkmate::check_integerish(as.numeric(Pkode_year[which(!grepl('[:alpha:]', Pkode_year))]),
                                                 lower = 1995,
                                                 upper = as.numeric(format(Sys.Date(), "%Y")),
                                                 any.missing = FALSE,
                                                 unique = TRUE),
                     # checkmate::check_character(Pkode_year, min.chars = 4, min.len = 1, any.missing = FALSE),
-                    checkmate::check_choice(Pkode_year, choices = c("last")))
+                    checkmate::check_choice(Pkode_year, choices = c("last")),
+                    add = checks)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+
 
 
   # READ IN ALL FILES IN THE DIRECTORY AND MAKE A LIST OF THE SELECTED VERSIONS OF EXTRACTS FROM PKODEREGISTERET
