8774dd557b8b3623eefd694bf661288836ee90dc
Petter Hopp <Petter.Hopp@vetinst.no>
2022-09-13 14:00:02 +0200

    refactor: implemented assert_read_functions in read_leveransereg

--- R/read_leveransereg.R
+++ R/read_leveransereg.R
@@ -28,23 +28,14 @@ read_leveransereg <- function(filename,
   from_path <- sub("/+$|\\\\+$", "", from_path)
 
   # ARGUMENT CHECKING ----
-  assert_read_function(filename = filename, from_path = from_path)
-
-  # # Argument checking
-  # # Object to store check-results
-  # checks <- checkmate::makeAssertCollection()
-  # # Perform checks
-  # checkmate::assert_character(filename, len = 1, min.chars = 1, add = checks)
-  # checkmate::assert_character(from_path, len = 1, min.chars = 1, add = checks)
-  # if (endsWith(from_path, "/")) {
-  #   checkmate::assert_directory_exists(substr(from_path, 1, nchar(from_path) - 1), access = "r", add = checks)
-  # } else {
-  #   checkmate::assert_directory_exists(from_path, access = "r", add = checks)
-  # }
-  # checkmate::assert_file_exists(paste0(from_path, filename), access = "r", add = checks)
-  # # Report check-results
-  # checkmate::reportAssertions(checks)
-
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  # Perform checks
+  checks <- assert_read_functions(filename = filename, from_path = from_path, add = checks)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+  # READ DATA ----
   # reads header and identifies characters by using NVIdb::standardize_columns
   colclasses <- standardize_columns(data = file.path(from_path, filename), property = "colclasses")
 
