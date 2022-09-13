f2317e55cd69d75e4052c4f08f2a46906c570e57
Petter Hopp <Petter.Hopp@vetinst.no>
2022-09-13 13:56:53 +0200

    refactor: implemented assert_read_functions in read_MT_omrader

--- R/read_MT_omrader.R
+++ R/read_MT_omrader.R
@@ -9,24 +9,14 @@ read_MT_omrader <- function(filename = list("komnr_2_MT_avdeling.csv", "MT_omrad
   from_path <- sub("/+$|\\\\+$", "", from_path)
 
   # ARGUMENT CHECKING ----
-  assert_read_function(filename = filename, from_path = from_path)
-
-  # # Argument checking
-  # # Object to store check-results
-  # checks <- checkmate::makeAssertCollection()
-  # # Perform checks
-  # checkmate::assert_list(filename, len = 2, types = "character", add = checks)
-  # checkmate::assert_character(from_path, len = 1, min.chars = 1, add = checks)
-  # if (endsWith(from_path, "/")) {
-  #   checkmate::assert_directory_exists(substr(from_path, 1, nchar(from_path) - 1), access = "r", add = checks)
-  # } else {
-  #   checkmate::assert_directory_exists(from_path, access = "r", add = checks)
-  # }
-  # checkmate::assert_file_exists(paste0(from_path, filename[1]), access = "r", add = checks)
-  # checkmate::assert_file_exists(paste0(from_path, filename[2]), access = "r", add = checks)
-  # # Report check-results
-  # checkmate::reportAssertions(checks)
-
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  # Perform checks
+  checks <- assert_read_functions(filename = filename, from_path = from_path, add = checks)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+  # READ DATA ----
   komnr_2_MT_avdeling <- read_csv_file(filename = filename[[1]],
                                        from_path = from_path,
                                        options = list(colClasses = "character", fileEncoding = "UTF-8"))
