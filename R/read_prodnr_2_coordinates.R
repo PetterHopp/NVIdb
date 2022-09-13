30dafde53c60b647c231cf82abf5c79b7a418551
Petter Hopp <Petter.Hopp@vetinst.no>
2022-09-13 14:00:38 +0200

    refactor: implemented assert_read_functions in read_prodnr_2_coordinates

--- R/read_prodnr_2_coordinates.R
+++ R/read_prodnr_2_coordinates.R
@@ -8,23 +8,14 @@ read_prodnr_2_coordinates <- function(filename = "Prodnr2Koordinater.csv",
   from_path <- sub("/+$|\\\\+$", "", from_path)
 
   # ARGUMENT CHECKING ----
-  assert_read_function(filename = filename, from_path = from_path)
-
-  # # Argument checking
-  # # Object to store check-results
-  # checks <- checkmate::makeAssertCollection()
-  # # Perform checks
-  # checkmate::assert_character(filename, len = 1, min.chars = 1, add = checks)
-  # checkmate::assert_character(from_path, len = 1, min.chars = 1, add = checks)
-  # if (endsWith(from_path, "/")) {
-  #   checkmate::assert_directory_exists(substr(from_path, 1, nchar(from_path) - 1), access = "r", add = checks)
-  # } else {
-  #   checkmate::assert_directory_exists(from_path, access = "r", add = checks)
-  # }
-  # checkmate::assert_file_exists(paste0(from_path, filename), access = "r", add = checks)
-  # # Report check-results
-  # checkmate::reportAssertions(checks)
-
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  # Perform checks
+  checks <- assert_read_functions(filename = filename, from_path = from_path, add = checks)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+  # READ DATA ----
   df1 <- read_csv_file(filename = filename,
                        from_path = from_path,
                        options = list(colClasses = "character",
