b041e09469776d1ebf0c4b455bb004e7c37e22a8
Petter Hopp <petter.hopp@vetinst.no>
2022-10-24 22:54:52 +0200

    doc: improved man-oxygen/position and man-oxygen/overwrite

--- man-roxygen/position.R
+++ man-roxygen/position.R
@@ -1,4 +1,5 @@
 #  template used for position in add-functions. 
 #' 
-#' @param position character input giving the position for the new columns, can be one
-#'      of c("first", "left", "right", "last", "keep"). The input can be abbreviated.
+#' @param position  [\code{character(1)}]. The position for the new columns, can 
+#'     be one of c("first", "left", "right", "last", "keep"). The input can be 
+#'     abbreviated, but must be unique, i.e. c("f", "le". "r", "la", "k").
