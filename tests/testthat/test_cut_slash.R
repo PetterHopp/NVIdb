df6a28d9fd84c12980df6c026770edae0e92d694
Petter Hopp <Petter.Hopp@vetinst.no>
2022-11-07 10:31:57 +0100

    style: styled spaces in test_cut_slash

--- tests/testthat/test_cut_slash.R
+++ tests/testthat/test_cut_slash.R
@@ -2,7 +2,7 @@ library(NVIdb)
 library(testthat)
 
 test_that("cut_slash", {
-expect_equal(cut_slash("C:/temp/"), "C:/temp") 
+expect_equal(cut_slash("C:/temp/"), "C:/temp")
 
 expect_equal(cut_slash("C:\\temp\\"), "C:\\temp")
 
@@ -10,4 +10,3 @@ expect_equal(cut_slash(c("C:/temp/", "C:\\temp\\")), c("C:/temp", "C:\\temp"))
 
 expect_equal(cut_slash(list("C:/temp/", "C:\\temp\\")), c("C:/temp", "C:\\temp"))
 })
-
