c96acbc27e27eabba0e61a106a373b76be46ec04
Petter Hopp <Petter.Hopp@vetinst.no>
2022-11-28 08:01:08 +0100

    feat: draft of get_holiday

---
+++ notes/get_holiday.R
@@ -0,0 +1,87 @@
+library(dplyr)
+year <- 2023
+
+get_holidays <- function (year, 
+                          type = "all", 
+                          trapped_days = "exclude", 
+                          invert = FALSE) {
+
+    ### ARGUMENT CHECKING ---- 
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  
+  # Perform checks
+  datasource <- NVIcheckmate::match_arg(x = type,
+                                        choices = c("easter", "holiday", "work", 
+                                                    "trapped", "weekend", "public", "sunday", "saturday",
+                                                    "non-moveable", "pentacost"),
+                                        several.ok = FALSE,
+                                        ignore.case = TRUE,
+                                        add = checks)
+  
+  # Report check-results
+  checkmate::reportAssertions(checks)
+  
+  ### NATIONAL HOLIDAYS ---- 
+  # Calculate Easter day
+  # reference
+  K <- floor(year/100)
+  M <- 15 + floor((3 * K + 3)/4) - floor((8 * K + 13)/25)
+  S <- 2 - floor((3 * K + 3)/4)
+  A <- year %% 19
+  D <- (19*A+M) %% 30
+  R <- floor((D+A/11)/29)
+  OG <- 21 + D - R
+  SZ <- 7 - ((year + floor(year/4)+S) %% 7) 
+  OE <- 7 - ((OG-SZ) %% 7) 
+  
+  easterday <- as.Date(paste0(year, "-03-01")) - 1 + OG + OE
+easter <- rep(easterday, 4) + c(-3, -2, 0, 1)
+pentacost <- rep(easterday, 3) + c(39, 49, 50)
+non_moveable <- as.Date(paste0(year, c("-01-01", "-05-01", "-05-17", "-12-25", "-12-26"))) 
+
+### CATEGORISE INTO HOLIDAYS ---- 
+dates  <- as.data.frame(matrix(data = c(as.Date(paste0(year, "-01-01")):as.Date(paste0(year, "-12-31"))),
+       dimnames = list(NULL, "date")))
+dates$date <- as.Date(dates$date, origin = "1970-01-01")
+dates <- dates %>%
+dplyr::mutate(weekday = lubridate::wday(.data$date, week_start=1)) %>%
+dplyr::mutate(holiday = dplyr::case_when(.data$weekday %in% c(6, 7) ~ as.character(.data$weekday),
+TRUE ~ "0" )) %>%
+dplyr::mutate(holiday = dplyr::case_when(.data$date %in% easter ~ "e",
+.data$date %in% pentacost ~ "p",
+.data$date %in% non_moveable ~ "n",
+TRUE ~ holiday)) %>% 
+dplyr::mutate(behind = dplyr::lag(holiday, 1)) %>%
+dplyr::mutate(ahead = dplyr::lead(holiday, 1)) %>%
+dplyr::mutate(holiday = dplyr::case_when(.data$ahead != 0 & .data$behind != 0 & .data$holiday == 0 ~ "t",
+TRUE ~ holiday)) 
+
+if ("easter" %in% type) {
+data[which(data$holiday == "e") , "select"] <- 1
+}
+if ("moving" %in% type) {
+data[which(data$holiday %in% c("e", "p")) , "select"] <- 1
+}
+if ("public" %in% type) {
+data[which(data$holiday %in% c("e", "p", "n")), "select"] <- 1
+}
+if ("sunday" %in% type) {
+data[which(data$weekday == 7) , "select"] <- 1
+}
+if ("saturday" %in% type) {
+data[which(data$weekday == 6), "select"] <- 1
+}
+if ("work" %in% type) {
+data[which(data$holiday %in% c("0", "t")), "select"] <- 1
+}
+if ("holiday" %in% type) {
+data[which(data$holiday %in% c("e", "p", "n", "6", "7")) , "select"] <- 1
+}
+if ("trapped" %in% type) {
+data[which(data$holiday %in% c("t")), "select"] <- 1
+}
+if ("raw" == type) {
+data[, "select"] <- 1
+}
+}
