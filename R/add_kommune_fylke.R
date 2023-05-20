24df1060d95745a3994a8cd0e577bdfa03b2e39c
Petter Hopp <petter.hopp@vetinst.no>
2023-05-20 22:41:59 +0200

    refactor: updated add_kommune_fylke utils::globalVariables moved to end of script.

--- R/add_kommune_fylke.R
+++ R/add_kommune_fylke.R
@@ -79,9 +79,6 @@
 #'                                             "kommune" = "gjeldende_kommune"))
 #' }
 #'
-# To avoid checking of the variable kommune_fylke as default input argument in the function
-utils::globalVariables("kommune_fylke")
-
 add_kommune_fylke <- function(data,
                               translation_table = kommune_fylke,
                               code_column = c("komnr"),
@@ -156,3 +153,7 @@ add_kommune_fylke <- function(data,
 
   return(data)
 }
+
+# To avoid checking of the variable kommune_fylke as default input argument in the function
+utils::globalVariables("kommune_fylke")
+
