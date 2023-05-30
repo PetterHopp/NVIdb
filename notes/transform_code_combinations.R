f896b7e20306e1144928feca80c500ef8dcb2aa6
Petter Hopp <petter.hopp@vetinst.no>
2023-05-30 04:39:37 +0200

    doc: created help for transform_code_combinations

--- notes/transform_code_combinations.R
+++ notes/transform_code_combinations.R
@@ -1,3 +1,57 @@
+#' @title Transform combinations of code values into new values 
+#' @description Transforms combinations of code values into new values in a data
+#'     frame. This is intended for use when only a few code value combinations
+#'     should be changed and one will avoid building translation tables or code
+#'     with several if, which or  case_when statements. In particularly it was 
+#'     inspired by the need of changing a few code combinations in PJS data when 
+#'     reporting surveillance programmes. 
+#' @details The function builds a transformation table based on the input. The 
+#'     from_values and the to_values give the data to a transformation table,
+#'     and the from_columns and the to_columns give the column names for the 
+#'     transformation table. 
+#'
+#' The from-values is a list of one or more vectors. Each vector represents one 
+#'     column variable with code values. The first  entry in each vector  
+#'     constitute one code combination to be transformed, the second entry 
+#'     constitutes the next code combinations. Likewise, is the to_values a list 
+#'     of one or more vectors. Each vector represents one column variable with 
+#'     code values to which the code combinations in the from_values should be 
+#'     transformed. 
+#'
+#' The from_columns is a vector of column names for the codes that should be
+#'     transformed. The column names will name each vector with code values in
+#'     the from_values list. The column names must exist in the data. Likewise,
+#'     the to_columns is a vector of column names for the columns with the
+#'     transformed code values. The to_columns can be the same as the 
+#'     from_columns, in which case the transformed combinations will replace
+#'     the original entries. If the to_columns don't exist in data, the column
+#'     will be added to the data. 
+#'     
+#' If the codes are not transformed, these will be kept in the data.
+#'     impute_when_missing_from gives the column names of the columns from which
+#'     to impute. Normally this will be the same as the from_columns. However,
+#'     if the number of columns in to_columns is less than in from_columns, it
+#'     will be necessary to give the columns from which to keep the code.
+#' 
+#' @param data \[\code{data.frame}\]\cr
+#' Data with code values that should be transformed. 
+#' @param from_values \[\code{list}\]\cr
+#' List with vector(s) of code values that should transformed, see details and examples.
+#' @param to_values \[\code{list}\]\cr
+#' List with vector(s) of code values that should be the results of the transformation, 
+#'     see details and examples.
+#' @param from_columns \[\code{character}\]\cr
+#' Column names for the code variables that should be transformed. 
+#' @param to_columns \[\code{character}\]\cr
+#' Column names for the code variables with the results of the transformation.
+#' @param impute_when_missing_from \[\code{character}\]\cr
+#' Column names for the code variables from which code values should be copied if no 
+#'     transformation is performed. Defaults to \code{from_columns}.
+#'
+#' @return A \code{data.frame}.
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @export
 
 
 transform_code_combinations <- function(data, 
