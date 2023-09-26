4e8d3ea6c05d5b0f9960a9d54563b8535b3aa2e6
Petter Hopp <Petter.Hopp@vetinst.no>
2023-09-26 07:38:27 +0200

    help: corrected NVIdb-deprecated Included version for deprecations. Corrected release date for add_produsent.

--- R/NVIdb-deprecated.R
+++ R/NVIdb-deprecated.R
@@ -3,7 +3,8 @@
 #'     versions of NVIdb only, and may be defunct as soon as the next release.
 #'     When possible, alternative functions are mentioned. Help pages for
 #'     deprecated functions are available at \code{help("<function>-deprecated")}.
-#' @details \code{add_produsent} was deprecated 2022-05-02 as other properties
+#' @details \code{add_produsent} was deprecated from v0.8.0 released 2022-08-25
+#'     as other properties
 #'     than 'gjeldende_prodnr8' could not be included without breaking backward
 #'     compatibility. Use \code{add_produsent_properties} instead and ensure
 #'     to set the parameter \code{impute_old_when_missing = TRUE} when translating
@@ -11,9 +12,9 @@
 #'     \code{impute_old_when_missing = FALSE} when translating from "prodnr8" to
 #'     other properties.
 #'
-#' \code{set_PAT}, \code{get_PAT}, and \code{remove_PAT} was deprecated 2023-08-22.
-#'     The functions were never taken into use. Functions from the much better
-#'     package \code{gitcreds} should be used instead.
+#' \code{set_PAT}, \code{get_PAT}, and \code{remove_PAT} was deprecated from
+#'     v0.11.0 released 2023-09-22. The functions were never taken into use.
+#'     Functions from the much better package \code{gitcreds} should be used instead.
 #'
 #' @param \dots (arguments)
 #' @return (results)
