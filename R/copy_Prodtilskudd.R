a0732e0486eceed128177196e9d33c1f2956a199
Petter Hopp <Petter.Hopp@vetinst.no>
2023-11-06 14:21:05 +0100

    feat: improved copy_Prodtilskudd Included argument extracted_date. Refactored argument checking.

--- R/copy_Prodtilskudd.R
+++ R/copy_Prodtilskudd.R
@@ -4,7 +4,13 @@
 copy_Prodtilskudd <- function(from_path = paste0(set_dir_NVI("Prodtilskudd"), "FormaterteData/"),
                               to_path = NULL,
                               Pkode_year = "last",
-                              Pkode_month = "both") {
+                              Pkode_month = "both",
+                              extracted_date = NULL) {
+
+  # PREPARE ARGUMENT ----
+  # Removing ending "/" and "\\" from pathnames
+  from_path <- sub("/+$|\\\\+$", "", from_path)
+  to_path <- sub("/+$|\\\\+$", "", to_path)
 
   # ARGUMENT CHECKING ----
   # Object to store check-results
@@ -12,27 +18,43 @@ copy_Prodtilskudd <- function(from_path = paste0(set_dir_NVI("Prodtilskudd"), "F
 
   # Perform checks
   # from_path
-  checkmate::assert_character(from_path, len = 1, min.chars = 1, add = checks)
-  if (endsWith(from_path, "/")) {
-    checkmate::assert_directory_exists(substr(from_path, 1, nchar(from_path) - 1), access = "r", add = checks)
-  } else {
-    checkmate::assert_directory_exists(from_path, access = "r", add = checks)
-  }
+  checkmate::assert_string(from_path, min.chars = 1, add = checks)
+  checkmate::assert_directory_exists(from_path, access = "r", add = checks)
   # to_path
-  if (endsWith(to_path, "/")) {
-    checkmate::assert_directory_exists(substr(to_path, 1, nchar(to_path) - 1), access = "r", add = checks)
-  } else {
-    checkmate::assert_directory_exists(to_path, access = "r", add = checks)
+  checkmate::assert_string(to_path, min.chars = 1, add = checks)
+  checkmate::assert_directory_exists(to_path, access = "r", add = checks)
+
+  if (is.null(extracted_date)) {
+    # Pkode_month
+    checkmate::assert_subset(Pkode_month, choices = c("both", "last", "01", "03", "05", "07", "10", "12"), add = checks)
+    # Pkode_year
+    checkmate::assert(checkmate::check_integerish(as.numeric(Pkode_year[grep('[[:alpha:]]', Pkode_year, invert = TRUE)]),
+                                                  lower = 1995,
+                                                  upper = as.numeric(format(Sys.Date(), "%Y")),
+                                                  any.missing = FALSE,
+                                                  all.missing = FALSE,
+                                                  unique = TRUE),
+                      # checkmate::check_character(Pkode_year, min.chars = 4, min.len = 1, any.missing = FALSE),
+                      checkmate::check_choice(Pkode_year, choices = c("last")),
+                      add = checks)
   }
-  checkmate::assert_subset(Pkode_month, choices = c("both", "last", "01", "03", "05", "07", "10", "12"), add = checks)
-  checkmate::assert(checkmate::check_integerish(as.numeric(Pkode_year[grep('[[:alpha:]]', Pkode_year, invert = TRUE)]),
-                                                lower = 1995,
-                                                upper = as.numeric(format(Sys.Date(), "%Y")),
-                                                any.missing = FALSE,
-                                                all.missing = FALSE,
-                                                unique = TRUE),
-                    checkmate::check_choice(Pkode_year, choices = c("last")),
-                    add = checks)
+  # If extracted_date != NULL, then input "both" and "last" are not accepted
+  if (!is.null(extracted_date)) {
+    # Pkode_month
+    NVIcheckmate::assert_subset_character(Pkode_month,
+                                          choices = c("01", "03", "05", "07", "10", "12"),
+                                          comment = "The inputs 'both' and 'last' are not accepted when 'extracted_date' is given",
+                                          add = checks)
+    # Pkode_year
+    NVIcheckmate::assert_integerish(as.numeric(Pkode_year[grep('[[:alpha:]]', Pkode_year, invert = TRUE)]),
+                                    lower = 1995,
+                                    upper = as.numeric(format(Sys.Date(), "%Y")),
+                                    any.missing = FALSE,
+                                    all.missing = FALSE,
+                                    unique = TRUE,
+                                    comment = "The input 'last' is not accepted when 'extracted_date' is given",
+                                    add = checks)
+  }
 
   # Report check-results
   checkmate::reportAssertions(checks)
@@ -42,7 +64,8 @@ copy_Prodtilskudd <- function(from_path = paste0(set_dir_NVI("Prodtilskudd"), "F
   # # READ IN ALL FILES IN THE DIRECTORY AND MAKE A LIST OF THE SELECTED VERSIONS OF EXTRACTS FROM PKODEREGISTERET ----
   filelist <- select_prodtilskudd_files(from_path = from_path,
                                         Pkode_year = Pkode_year,
-                                        Pkode_month = Pkode_month)
+                                        Pkode_month = Pkode_month,
+                                        extracted_date = extracted_date)
 
   # COPY FILES ----
   for (i in c(1:dim(filelist)[1])) {
