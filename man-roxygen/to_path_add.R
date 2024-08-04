78fa53f9e9427149086a0474a999d7b74b23049a
Petter Hopp <petter.hopp@vetinst.no>
2024-08-04 20:11:42 +0200

    refactor: created template to_path_add

---
+++ man-roxygen/to_path_add.R
@@ -0,0 +1,5 @@
+#  template used for to_path in add-function help. 
+#' 
+#' @param to_path [\code{character(1)}]\cr
+#' Path to which the source files used to generate the translation table should
+#'    be copied. Defaults to \code{NULL}.
