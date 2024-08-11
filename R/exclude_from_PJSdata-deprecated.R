e8b99fff63ec2c07c74454b55b297a0894c6c16d
Petter Hopp <petter.hopp@vetinst.no>
2024-08-11 07:57:32 +0200

    refactor: deprecate exclude_from_PJSdata Created help for deprecated. Issues warnings if used.

--- R/exclude_from_PJSdata-deprecated.R
+++ R/exclude_from_PJSdata-deprecated.R
@@ -22,53 +22,89 @@
 #' @return data frame without excluded PJS-data.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
-#' @export
+#' @name exclude_from_PJSdata-deprecated
+#' @keywords internal
+#'
 #' @examples
 #' \dontrun{
 #' # cleaning sak_m_res
-#'  sak_m_res <- exclude_from_PJSdata(PJSdata = sak_m_res,
+#' sak_m_res <- exclude_from_PJSdata(PJSdata = sak_m_res,
 #'                                    abroad = "exclude",
 #'                                    quality = "exclude")
 #' }
 #'
+NULL
+
+#' @title exclude_from_PJSdata is Deprecated
+#' @description \code{exclude_from_PJSdata} was deprecated in NVIdb v0.13.0 released
+#'     2024-##-##. All PJS related functions have been moved to \code{NVIpjsr}.
+#'     Use \code{NVIpjsr::exclude_from_PJSdata} instead. When attaching packages,
+#'     remember to attach \code{NVIdb} before \code{NVIpjsr}.
+#' @details The old help pages can be found at \code{help("exclude_from_PJSdata-deprecated")}.
+#'     Information on deprecated function can be found at \code{help("NVIdb-deprecated")}.
+#'
+#' @param PJSdata Data frame with data extracted from PJS.
+#' @param abroad If equal "exclude", samples from abroad are excluded. Allowed
+#'     values are c("exclude", "include").
+#' @param quality If equal "exclude", samples registered as quality assurance
+#'     and ring trials are excluded. Allowed values are c("exclude", "include").
+#'
+#' @export
+#' @keywords internal
+#'
+
 exclude_from_PJSdata <- function(PJSdata, abroad = "exclude", quality = "exclude") {
 
-  # ARGUMENT CHECKING ----
-  # Object to store check-results
-  checks <- checkmate::makeAssertCollection()
+  # DEPRECATED ----
+  .Deprecated(new = "exclude_from_PJSdata",
+              package = "NVIdb",
+              msg = paste("'exclude_from_PJSdata' is replaced by
+                          'NVIpjsr::exclude_from_PJSdata'"))
 
-  # Perform checks
-  # pjsDATA
-  checkmate::assert_data_frame(PJSdata, add = checks)
-  # abroad
-  checkmate::assert_choice(abroad, choices = c("exclude", "include"), add = checks)
-  # quality
-  checkmate::assert_choice(quality, choices = c("exclude", "include"), add = checks)
+  if (isTRUE(NVIcheckmate::check_package(x = "NVIpjsr", type = "installed"))) {
+    select_statement <- NVIpjsr::exclude_from_PJSdata(PJSdata = PJSdata,
+                                                      abroad = abroad,
+                                                      quality = quality)
 
-  # Report check-results
-  checkmate::reportAssertions(checks)
+    return(select_statement)
+  } else {
+    # ARGUMENT CHECKING ----
+    # Object to store check-results
+    checks <- checkmate::makeAssertCollection()
+
+    # Perform checks
+    # pjsDATA
+    checkmate::assert_data_frame(PJSdata, add = checks)
+    # abroad
+    checkmate::assert_choice(abroad, choices = c("exclude", "include"), add = checks)
+    # quality
+    checkmate::assert_choice(quality, choices = c("exclude", "include"), add = checks)
+
+    # Report check-results
+    checkmate::reportAssertions(checks)
 
 
-  # PERFORM CLEANING ----
-  # Remove samples from abroad
-  if (abroad == "exclude") {
-    # Remove eier_lokalitet with landnr different from Norway in address
+    # PERFORM CLEANING ----
+    # Remove samples from abroad
+    if (abroad == "exclude") {
+      # Remove eier_lokalitet with landnr different from Norway in address
 
-    PJSdata <- subset(PJSdata,
-                      PJSdata$eier_lokalitettype != "LAND" |
-                        is.na(PJSdata$eier_lokalitettype) |
-                        (PJSdata$eier_lokalitettype == "LAND" & PJSdata$eier_lokalitetnr == "NO"))
+      PJSdata <- subset(PJSdata,
+                        PJSdata$eier_lokalitettype != "LAND" |
+                          is.na(PJSdata$eier_lokalitettype) |
+                          (PJSdata$eier_lokalitettype == "LAND" & PJSdata$eier_lokalitetnr == "NO"))
 
+    }
+
+
+    if (quality == "exclude") {
+      # Delete qualty assurance
+      PJSdata <- subset(PJSdata,
+                        substr(PJSdata$hensiktkode, 1, 2) != "09" |
+                          is.na(PJSdata$hensiktkode))
+
+    }
+
+    return(PJSdata)
   }
-
-
-  if (quality == "exclude") {
-    # Delete qualty assurance
-    PJSdata <- subset(PJSdata,
-                      substr(PJSdata$hensiktkode, 1, 2) != "09" |
-                        is.na(PJSdata$hensiktkode))
-
-  }
-
-  return(PJSdata)
 }
