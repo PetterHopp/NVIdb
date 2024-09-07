549fff096e3670b5fc08f886a30b5e65b16f8107
Petter Hopp <petter.hopp@vetinst.no>
2024-09-07 21:51:56 +0200

    refactor: updated read_PJS_code_2_text Replaced read_csv_file with utils::read.csv2. Updated help.

--- R/read_PJS_code_2_text.R
+++ R/read_PJS_code_2_text.R
@@ -2,7 +2,8 @@
 #' @rdname add_PJS_code_description
 
 read_PJS_codes_2_text <- function(filename = "PJS_codes_2_text.csv",
-                                  from_path = paste0(set_dir_NVI("Provedata_Rapportering"), "FormaterteData/")) {
+                                  from_path = paste0(set_dir_NVI("Provedata_Rapportering"), "FormaterteData/"),
+                                  ...) {
 
   # Removing ending "/" and "\\" from pathnames
   from_path <- sub("/+$|\\\\+$", "", from_path)
@@ -16,9 +17,13 @@ read_PJS_codes_2_text <- function(filename = "PJS_codes_2_text.csv",
   checkmate::reportAssertions(checks)
 
   # READ DATA ----
-  PJS_codes_2_text <- read_csv_file(filename = filename,
-                                    from_path = from_path,
-                                    options = list(colClasses = "character", fileEncoding = "UTF-8"))
+  # PJS_codes_2_text <- read_csv_file(filename = filename,
+  #                                   from_path = from_path,
+  #                                   options = list(colClasses = "character", fileEncoding = "UTF-8"))
+  PJS_codes_2_text <- utils::read.csv2(file = file.path(from_path, filename),
+                                       colClasses = "character",
+                                       fileEncoding = "UTF-8",
+                                       ...)
 
   # Remove double "" that have replaced single when saving as csv-file
   PJS_codes_2_text$navn <- gsub('\"\"', "\"", PJS_codes_2_text$navn)
