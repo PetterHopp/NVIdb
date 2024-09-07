292633118af3fe1850c0c582f2259fd1e1d7641a
Petter Hopp <petter.hopp@vetinst.no>
2024-09-07 22:32:03 +0200

    style: corrected language in template/to_path_add

--- man-roxygen/to_path_add.R
+++ man-roxygen/to_path_add.R
@@ -1,5 +1,5 @@
 #  template used for to_path in add-functions. 
 #' 
 #' @param to_path [\code{character(1)}]\cr
-#' Path to which the source file(s) used to generate the translation table should
+#' Path to where the source file(s) used to generate the translation table should
 #'     be copied.
