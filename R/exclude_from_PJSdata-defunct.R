c6e770c06960782a338555b4fc12795d2ec5de12
Petter Hopp <petter.hopp@vetinst.no>
2025-12-19 08:08:19 +0100

    refactor: corrected name to exclude_from_PJSdata-defunct

--- R/exclude_from_PJSdata-defunct.R
+++ R/exclude_from_PJSdata-defunct.R
@@ -22,34 +22,7 @@
 #' @return data frame without excluded PJS-data.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
-#' @name exclude_from_PJSdata-deprecated
-#' @keywords internal
-#'
-#' @examples
-#' \dontrun{
-#' # cleaning sak_m_res
-#' sak_m_res <- exclude_from_PJSdata(PJSdata = sak_m_res,
-#'                                    abroad = "exclude",
-#'                                    quality = "exclude")
-#' }
-#'
-NULL
-
-#' @title exclude_from_PJSdata is Deprecated
-#' @description \code{exclude_from_PJSdata} was deprecated in NVIdb v0.13.0 released
-#'     2024-##-##. All PJS related functions have been moved to \code{NVIpjsr}.
-#'     Use \code{NVIpjsr::exclude_from_PJSdata} instead. When attaching packages,
-#'     remember to attach \code{NVIdb} before \code{NVIpjsr}.
-#' @details The old help pages can be found at \code{help("exclude_from_PJSdata-deprecated")}.
-#'     Information on deprecated function can be found at \code{help("NVIdb-deprecated")}.
-#'
-#' @param PJSdata Data frame with data extracted from PJS.
-#' @param abroad If equal "exclude", samples from abroad are excluded. Allowed
-#'     values are c("exclude", "include").
-#' @param quality If equal "exclude", samples registered as quality assurance
-#'     and ring trials are excluded. Allowed values are c("exclude", "include").
-#'
-#' @name exclude_from_PJSdata-dedefunct
+#' @name exclude_from_PJSdata-defunct
 #' @keywords internal
 #'
 NULL
